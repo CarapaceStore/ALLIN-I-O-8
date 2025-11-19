0478e39b6a7b61322762e9ac37416ec5b1071d6a
ALLIN Systeme <store@carapaceamenagement.fr>
2025-11-19 10:39:10 +0100

    Update config_flow.py

--- custom_components/allin_io_8/config_flow.py
+++ custom_components/allin_io_8/config_flow.py
@@ -1,74 +1,52 @@
-"""Config flow for ALLIN integration."""
+"""Config flow pour l'intégration ALLIN I/O 8 (KMTronic)."""
+
+from __future__ import annotations
+
 import logging
 
-import aiohttp
-from pykmtronic.auth import Auth
-from pykmtronic.hub import KMTronicHubAPI
+from homeassistant import config_entries
 import voluptuous as vol
 
-from homeassistant import config_entries, core, exceptions
-from homeassistant.helpers import aiohttp_client
-
-from .const import CONF_HOSTNAME, CONF_PASSWORD, CONF_USERNAME
-from .const import DOMAIN  # pylint:disable=unused-import
+from .const import (
+    DOMAIN,
+    CONF_HOSTNAME,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema({CONF_HOSTNAME: str, CONF_USERNAME: str, CONF_PASSWORD: str})
 
-
-async def validate_input(hass: core.HomeAssistant, data):
-    """Validate the user input allows us to connect."""
-
-    session = aiohttp_client.async_get_clientsession(hass)
-    auth = Auth(
-        session,
-        f"http://{data[CONF_HOSTNAME]}",
-        data[CONF_USERNAME],
-        data[CONF_PASSWORD],
-    )
-    hub = KMTronicHubAPI(auth)
-
-    try:
-        await hub.async_get_status()
-    except aiohttp.client_exceptions.ClientResponseError as err:
-        raise InvalidAuth from err
-    except aiohttp.client_exceptions.ClientConnectorError as err:
-        raise CannotConnect from err
-
-    return data
-
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for kmtronic."""
+class AllinIO8ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Gestion du flux de configuration pour ALLIN I/O 8."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        errors = {}
+        """Première étape de config (écran principal)."""
+        errors: dict[str, str] = {}
+
         if user_input is not None:
-            try:
-                info = await validate_input(self.hass, user_input)
+            # on utilise l'hôte comme identifiant unique
+            await self.async_set_unique_id(user_input[CONF_HOSTNAME])
+            self._abort_if_unique_id_configured()
 
-                return self.async_create_entry(title=info["host"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+            # on crée l'entrée de configuration telle quelle
+            return self.async_create_entry(
+                title=user_input[CONF_HOSTNAME],
+                data=user_input,
+            )
 
-        return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_HOSTNAME): str,
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
+            }
         )
 
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
+        )
